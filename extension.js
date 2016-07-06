99066d4b7b18f6f93669303d8d615f2c7a8d1cee
james-johnston <jaj@darkblack.org>
2016-07-06 19:56:11 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -8,7 +8,7 @@ new (function() {
 		url: 'https://github.com/savaka2/scratch-extensions/wiki/Link-Opener-extension'
 	};
 	
-	var url = window.location.href;
+	var siteUrl = window.location.href;
 	
 	ext._shutdown = function() {};
 	
@@ -23,7 +23,7 @@ new (function() {
 			type: 'POST',
 			url: url,
 			success: function(response) {
-				alert(url);
+				alert(siteUrl);
 			}
 		});
 	};
