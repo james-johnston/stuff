f47b6c01407561c504614ecef93fde6d86354155
james-johnston <jaj@darkblack.org>
2016-07-07 20:59:55 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -1,33 +1,45 @@
 new (function() {
 	var ext = this;
-	var descriptor = {
-		blocks: [
-			[' ', 'open link %s in new window', 'openLinkNewTab', 'http://scratch.mit.edu/'],
-			[' ', 'digital pin 1 setting %n', 'setDigital', '0'],
-		],
-		url: 'https://github.com/savaka2/scratch-extensions/wiki/Link-Opener-extension'
-	};
 	
-	var siteUrl = window.location.href;
+	var descriptor = {
+    	blocks: [
+      	[' ', 'digital pin %m.pin setting %m.dsetting', 'setDigital', '1', 'off'],
+      	[' ', 'pwm pin %m.ppin setting %n', 'setPwm', '1', '100'],
+      	[' ', 'digital pin %m.pin get', 'getDigital', '1'],
+      	[' ', 'pwm pin %m.ppin get', 'getPwm', '1']
+	    ],
+	    'menus': {
+	    	'pin': ['1', '2', '3', '4'],
+	      	'dsetting': ['on', 'off'],
+	      	'ppin': ['1', '2']
+		},
+	 	url: 'https://github.com/savaka2/scratch-extensions/wiki/Link-Opener-extension'
+	};
 	
 	ext._shutdown = function() {};
 	
 	ext._getStatus = function() {
-		return {status:2, msg:'Ready'};
-	};
-	
-	ext.setDigital = function(setting) {
-		var url = "http://192.168.2.102/gpio2/" + setting;
+    	return {status:2, msg:'Ready'};
+  	};
+  	
+  	ext.getPwm = function(pin) {
+  	};
+  	
+  	ext.setPwm = function(pin, setting) {
+  	};
+  	
+  	ext.getDigital = function(pin) {
+  	};
+  	
+  	ext.setDigital = function(setting, url) {
+    	var url = 'http://%%%%%%/gpio2/' + setting; 
 		console.log('setting' + setting);
 		$.ajax({
 			type: 'POST',
 			url: url,
 			success: function(response) {
-				alert(siteUrl);
-			}
+		}
 		});
 	};
-	
-	
-	ScratchExtensions.register('Link Opener', descriptor, ext);
-})();
+
+ScratchExtensions.register('Link Opener', descriptor, ext);})();
